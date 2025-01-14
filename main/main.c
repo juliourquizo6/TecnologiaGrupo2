662813f20f621fe77e8ddca11a96730adca31159
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-14 21:58:12 +0100

    Thingsboard main test

--- main/main.c
+++ main/main.c
@@ -1,34 +1,21 @@
-#include <stdio.h>
-#include "esp_wifi.h"
-#include <wifi_provisioning/manager.h>
-#include <wifi_provisioning/scheme_softap.h>
-#include <wifi_component.h>
-
-
-static esp_event_loop_handle_t event_loop_handle;
-
-
+#include <esp_event.h>
+#include <esp_netif.h>
+#include <nvs_flash.h>
+#include <protocol_examples_common.h>
+#include <tb.h>
 
 void app_main(void)
 {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
 
-    esp_event_loop_args_t event_loop_args = {
-        .queue_size = 10,
-        .task_name = "esp_event_loop_run_task",
-        .task_priority = 5,
-        .task_stack_size = 2048,
-        .task_core_id = tskNO_AFFINITY,
-    };
+    thingsboard *tb = NULL;
 
-    esp_event_loop_create(&event_loop_args, &event_loop_handle);
+    ESP_ERROR_CHECK(thingsboard_init(&tb, NULL, 0, NULL, NULL));
+    ESP_ERROR_CHECK(tb_connect(tb));
+    ESP_ERROR_CHECK(tb_send_telemetry(tb, "{\"temperature\": 25.0}"));
 
-    wifi_init();
-    
-    // 1: Provisionamiento
-    
-    // 2: Coneccion WiFi
-    
-    //2.2: Tarea Sensor
-    
-
+    tb_destroy(tb);
 }
