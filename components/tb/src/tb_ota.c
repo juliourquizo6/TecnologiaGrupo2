9355e71776cde2270f9e2bcdaeec02e02c756083
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-16 00:36:48 +0100

    Define https ota update

--- components/tb/src/tb_ota.c
+++ components/tb/src/tb_ota.c
@@ -1,14 +1,32 @@
 #include <assert.h>
+#include "esp_https_ota.h"
+#include "esp_system.h"
 #include "tb/tb_ota.h"
 
 static void tb_ota_subscribe_to_update_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
 
-esp_err_t tb_ota_update(thingsboard *tb)
+esp_err_t tb_ota_update(const char *url, const char *cert_pem)
 {
-    assert(tb);
+    assert(cert_pem);
 
     esp_err_t err = ESP_OK;
 
+    esp_http_client_config_t https_cfg = {
+        .url = url,
+        .cert_pem = cert_pem,
+    };
+    esp_https_ota_config_t ota_cfg = {
+        .http_config = &https_cfg,
+    };
+    err = esp_https_ota(&ota_cfg);
+    if (err != ESP_OK)
+    {
+        goto cleanup;
+    }
+
+    esp_restart();
+
+cleanup:
     return err;
 }
 
