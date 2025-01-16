35f003b874ccd1ad6642e485f8fb2712d7bb9229
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-16 10:48:46 +0100

    Update token length definitions and modify token handling functions for consistency

--- components/tb/include/tb/tb_conn.h
+++ components/tb/include/tb/tb_conn.h
@@ -4,7 +4,10 @@
 #include "esp_err.h"
 #include "tb/tb.h"
 
+#define TB_CONN_TOKEN_LENGTH_MIN 1
+#define TB_CONN_TOKEN_LENGTH_MAX 32
+
 esp_err_t tb_conn_connect(thingsboard *tb);
-esp_err_t tb_conn_disconnect(thingsboard *tb);
+esp_err_t tb_conn_disconnect(const thingsboard *tb);
 
 #endif
--- components/tb/include/tb/tb_nvs.h
+++ components/tb/include/tb/tb_nvs.h
@@ -6,11 +6,10 @@
 
 #define TB_NVS_NAMESPACE_NAME "tb"
 #define TB_NVS_TOKEN_KEY "token"
-#define TB_NVS_TOKEN_LENGTH_MIN 2
-#define TB_NVS_TOKEN_LENGTH_MAX 33
 
-esp_err_t tb_nvs_get_token(char *token, size_t *token_length);
-esp_err_t tb_nvs_set_token(const char *token);
 esp_err_t tb_nvs_has_token(bool *has_token);
+esp_err_t tb_nvs_get_token(char *token, size_t token_length);
+esp_err_t tb_nvs_set_token(const char *token);
+esp_err_t tb_nvs_erase_token(void);
 
 #endif
