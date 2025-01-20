f4259b67d3c38ae477027f54a1c9d17a05fd9959
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-20 13:42:31 +0100

    Declare new api

---
+++ components/tb/tb.h
@@ -0,0 +1,34 @@
+#include "esp_err.h"
+#include "esp_event.h"
+#include "mqtt_client.h"
+
+#define CONFIG_TB_DEVICE_NAME ""
+#define CONFIG_TB_PROVISION_DEVICE_KEY ""
+#define CONFIG_TB_PROVISION_DEVICE_SECRET ""
+
+#define ACCESS_TOKEN_LEN_MAX 32
+
+ESP_EVENT_DECLARE_BASE(TB_EVENTS);
+
+enum
+{
+    TB_EVENT_ATTRIBUTES,
+};
+
+typedef struct thingsboard thingsboard;
+
+esp_err_t tb_create(thingsboard *tb, const char *host, const char *topic, char *cert_pem, esp_event_handler_t event_handler, void *event_handler_arg);
+
+esp_err_t tb_delete(thingsboard *tb);
+
+esp_err_t tb_send(const thingsboard *tb, const char *data);
+
+struct thingsboard
+{
+    char *host;
+    char *topic;
+    char *cert_pem;
+    char access_token[ACCESS_TOKEN_LEN_MAX + 1];
+    esp_event_loop_handle_t event_loop;
+    esp_mqtt_client_handle_t mqtt_client;
+};
