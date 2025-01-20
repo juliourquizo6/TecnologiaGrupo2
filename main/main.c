9e42013f21916bed6748bb683521d3a34eaa3dd0
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-20 13:42:21 +0100

    Use new api

--- main/main.c
+++ main/main.c
@@ -2,7 +2,11 @@
 #include "esp_netif.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
-#include "thingsboard.h"
+#include "tb.h"
+
+static void tb_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+
+static thingsboard tb = {0};
 
 void app_main(void)
 {
@@ -11,17 +15,24 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
-    esp_event_loop_args_t event_loop_cfg = {
-        .queue_size = 10,
-        .task_stack_size = 1000,
-    };
-    esp_event_loop_handle_t event_loop = NULL;
-    ESP_ERROR_CHECK(esp_event_loop_create(&event_loop_cfg, &event_loop));
+    ESP_ERROR_CHECK(tb_create(&tb, NULL, NULL, NULL, tb_event_handler, NULL));
+}
 
-    thingsboard tb = {0};
-    ESP_ERROR_CHECK(tb_init(&tb, event_loop, NULL, 0, NULL, NULL));
-    ESP_ERROR_CHECK(tb_conn_start(&tb));
-    ESP_ERROR_CHECK(tb_tm_send(&tb, "{\"temperature\": 25.0}"));
+void tb_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    thingsboard *tb = (thingsboard *)event_data;
+    (void)tb;
 
-    tb_destroy(&tb);
+    switch (event_id)
+    {
+    case TB_EVENT_ATTRIBUTES:
+    {
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
 }
