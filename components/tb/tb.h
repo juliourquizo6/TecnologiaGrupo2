f684faf51357e2d2bcdc879af704221efd8f21ae
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-21 01:44:30 +0100

    Update tb_client_init and tb_client_send function signatures for improved clarity and consistency

--- components/tb/tb.h
+++ components/tb/tb.h
@@ -1,6 +1,6 @@
 #include "cJSON.h"
 #include "esp_err.h"
 
-esp_err_t tb_client_init(const char *hostname, const char *certificate, const char *telemetry_topic, void (*attributes_callback)(const cJSON *));
+esp_err_t tb_client_init(const char *hostname, const char *mqtt_client_certificate, const char *http_client_certificate, const char *telemetry_topic, void (*attributes_callback)(const cJSON *));
 
-esp_err_t tb_client_send(const char *data);
+esp_err_t tb_client_send(const cJSON *data);
