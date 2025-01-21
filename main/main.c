3fc2a90fedd340fcf33223f58abb8b892889fe2f
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-21 01:03:58 +0100

    Use new simplified api

--- main/main.c
+++ main/main.c
@@ -4,10 +4,6 @@
 #include "protocol_examples_common.h"
 #include "tb.h"
 
-static void tb_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-
-static thingsboard tb = {0};
-
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -15,24 +11,5 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
-    ESP_ERROR_CHECK(tb_create(&tb, NULL, NULL, NULL, tb_event_handler, NULL));
-}
-
-void tb_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    thingsboard *tb = (thingsboard *)event_data;
-    (void)tb;
-
-    switch (event_id)
-    {
-    case TB_EVENT_ATTRIBUTES:
-    {
-        break;
-    }
-
-    default:
-    {
-        break;
-    }
-    }
+    ESP_ERROR_CHECK(tb_client_init(NULL, NULL, NULL, NULL));
 }
