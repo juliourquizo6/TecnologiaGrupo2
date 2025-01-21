2b954fc387f02dd6de231fd1d406a7ec8af5e07e
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-21 02:35:39 +0100

    Remove tb.h and update main.c to include tb_client.h for improved modularity

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
 #include "esp_netif.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
-#include "tb.h"
+#include "tb_client.h"
 
 void app_main(void)
 {
@@ -10,6 +10,5 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
-
-    ESP_ERROR_CHECK(tb_client_init(NULL, NULL, NULL, NULL));
+    ESP_ERROR_CHECK(tb_client_init(NULL, NULL, NULL));
 }
