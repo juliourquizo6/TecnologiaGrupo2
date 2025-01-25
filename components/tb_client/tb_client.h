53bede70390f3a931fcb15107b2663e4ae43ba05
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-25 09:45:31 +0100

    Add header guards

--- components/tb_client/tb_client.h
+++ components/tb_client/tb_client.h
@@ -1,6 +1,11 @@
+#ifndef TB_CLIENT_H
+#define TB_CLIENT_H
+
 #include "cJSON.h"
 #include "esp_err.h"
 
 esp_err_t tb_client_init(const char *host, const char *topic, void (*callback)(const cJSON *));
 
 void tb_client_send(const cJSON *data);
+
+#endif
