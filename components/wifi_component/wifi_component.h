ecddd96a7de0bee52f9f262fc8d8d099c68613a9
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-27 01:55:59 +0100

    Fix missing identifier

--- components/wifi_component/wifi_component.h
+++ components/wifi_component/wifi_component.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <esp_err.h>
+#include <esp_event.h>
 
 // Functions to get security parameters
 esp_err_t example_get_sec2_salt(const char **salt, uint16_t *salt_len);
@@ -12,7 +13,7 @@ esp_err_t load_thingsboard_url(char *url, size_t max_len);
 void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
 
 // Function to handle ThingsBoard URL received during provisioning
-esp_err_t thingsboard_url_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen, 
+esp_err_t thingsboard_url_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen,
                                   uint8_t **outbuf, ssize_t *outlen, void *priv_data);
 
 // Function to generate the device service name
