49190481f48c0f23d0c39db341febe22853528c4
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-27 01:58:01 +0100

    Use tb url

--- main/main.c
+++ main/main.c
@@ -2,39 +2,40 @@
 #include "esp_log.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-#include "protocol_examples_common.h"
+#include "nvs_flash.h"
 #include "sensor_co2.h"
 #include "tb_client.h"
 #include "wifi_component.h"
 
-static const char *host = NULL;
-static const char *topic = NULL;
-static void sgp30_handler(cJSON *data_sensor_co2)
-{
-    tb_client_send_telemetry(data_sensor_co2);
-}
-
 static void attributes_callback(const cJSON *attributes)
 {
     cJSON *sampling_rate = cJSON_GetObjectItem(attributes, "samplingRate");
+    if (!sampling_rate)
+    {
+        return;
+    }
 
     if (!cJSON_IsNumber(sampling_rate))
     {
         return;
     }
 
-    int hz = sampling_rate->valueint;
-    int ms = 1000 / hz;
+    int sampling_rate_hz = sampling_rate->valueint;
 
-    sensor_co2_set_frecuencia_lectura(ms);
+    int sampling_rate_ms = 1000 / sampling_rate_hz;
+
+    sensor_co2_set_frecuencia_lectura(sampling_rate_ms);
 }
+
 void app_main(void)
 {
-    ESP_ERROR_CHECK(nvs_initialize());
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    provision_and_connect();
 
-    tb_client_init(host, topic, attributes_callback);
-    sensor_co2_create(sgp30_handler);
+    provision_and_connect();
+    // TODO
+    const char *topic = NULL;
+    tb_client_init(thingsboard_url, topic, attributes_callback);
+    sensor_co2_create(tb_client_send_telemetry);
 }
