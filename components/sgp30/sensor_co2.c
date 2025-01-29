aecd368076fb532dcedb1606df7d89732717ed17
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-29 22:58:26 +0100

    Fix sync issues with tasks

--- components/sgp30/sensor_co2.c
+++ components/sgp30/sensor_co2.c
@@ -7,18 +7,21 @@
 #include <esp_timer.h>
 #include <esp_log.h>
 #include "sensor_co2.h"
+#include "freertos/event_groups.h"
 
-#define T_LECTURA   CONFIG_T_LECTURA
-#define T_ENVIO     CONFIG_T_ENVIO
+#define T_LECTURA CONFIG_T_LECTURA
+#define T_ENVIO CONFIG_T_ENVIO
 #define CALIBRATION 5
+#define TIMERS_READY_BIT BIT0
 
-static const char* TAG = "SGP30";
+static const char *TAG = "SGP30";
 
 static i2c_port_t i2c_num = I2C_MASTER_NUM;
 sgp30_dev_t sgp30_sensor;
 esp_timer_handle_t tm_lectura_handle;
 esp_timer_handle_t tm_envio_handle;
 sensor_co2_handler sensor_handler;
+static EventGroupHandle_t timers_event_group;
 
 struct data_sensor_co2
 {
@@ -33,15 +36,16 @@ static size_t count = 0;
 
 /* I2C */
 // Inicializacion
-static void i2c_init(void) {
+static void i2c_init(void)
+{
     int i2c_master_port = I2C_MODE_MASTER;
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,         // select GPIO specific to your project
+        .sda_io_num = I2C_MASTER_SDA_IO, // select GPIO specific to your project
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_io_num = I2C_MASTER_SCL_IO,         // select GPIO specific to your project
+        .scl_io_num = I2C_MASTER_SCL_IO, // select GPIO specific to your project
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,  // select frequency specific to your project
+        .master.clk_speed = I2C_MASTER_FREQ_HZ, // select frequency specific to your project
     };
 
     // Configuracion
@@ -49,24 +53,25 @@ static void i2c_init(void) {
 
     // Instalacion del driver
     ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0));
-
 }
 
-
-int8_t main_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) { // *intf_ptr = dev->intf_ptr
+int8_t main_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{                   // *intf_ptr = dev->intf_ptr
     int8_t ret = 0; /* Return 0 for Success, non-zero for failure */
 
-    if (len == 0) {
+    if (len == 0)
+    {
         return ESP_OK;
     }
 
-    uint8_t chip_addr = *(uint8_t*)intf_ptr;
+    uint8_t chip_addr = *(uint8_t *)intf_ptr;
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
     i2c_master_start(cmd);
 
-    if (reg_addr != 0xff) {
+    if (reg_addr != 0xff)
+    {
         i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
         i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
         i2c_master_start(cmd);
@@ -74,7 +79,8 @@ int8_t main_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *in
 
     i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
 
-    if (len > 1) {
+    if (len > 1)
+    {
         i2c_master_read(cmd, reg_data, len - 1, ACK_VAL);
     }
     i2c_master_read_byte(cmd, reg_data + len - 1, NACK_VAL);
@@ -87,17 +93,18 @@ int8_t main_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *in
     return ret;
 }
 
-
-int8_t main_i2c_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+int8_t main_i2c_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
     int8_t ret = 0; /* Return 0 for Success, non-zero for failure */
 
-    uint8_t chip_addr = *(uint8_t*)intf_ptr;
+    uint8_t chip_addr = *(uint8_t *)intf_ptr;
 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
 
-    if (reg_addr != 0xFF) {
+    if (reg_addr != 0xFF)
+    {
         i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
     }
 
@@ -111,18 +118,18 @@ int8_t main_i2c_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *i
     return ret;
 }
 
-
 /* TIMERS */
 // Callbacks
-static void tm_lectura(void* arg) {
+static void tm_lectura(void *arg)
+{
     sgp30_IAQ_measure(&sgp30_sensor);
     count++;
     data.eCO2 = data.eCO2 + (sgp30_sensor.eCO2 - data.eCO2) / count;
     data.TVOC = data.TVOC + (sgp30_sensor.TVOC - data.TVOC) / count;
 }
 
-
-static void tm_envio(void* arg) {
+static void tm_envio(void *arg)
+{
     cJSON *datos = cJSON_CreateObject();
     // Calibrated for ethanol (https://www.catsensors.com/media/pdf/Sensor_Sensirion_IAM.pdf)
     cJSON_AddNumberToObject(datos, "tvoc", (double)data.TVOC * (110.0 / (0.0244 * 1000.0)) / 1000.0);
@@ -134,37 +141,36 @@ static void tm_envio(void* arg) {
     count = 0;
 }
 
-
 // Inicializacion
-static void timers_init(void) {
+static void timers_init(void)
+{
     // Timer lectura
     const esp_timer_create_args_t args_tm_lectura = {
         .callback = &tm_lectura,
-        .name = "Tiempo de lectura"
-    };
+        .name = "Tiempo de lectura"};
 
     ESP_ERROR_CHECK(esp_timer_create(&args_tm_lectura, &tm_lectura_handle));
 
     // Timer envio
     const esp_timer_create_args_t args_tm_envio = {
         .callback = &tm_envio,
-        .name = "Tiempo de envio"
-    };
+        .name = "Tiempo de envio"};
 
     ESP_ERROR_CHECK(esp_timer_create(&args_tm_envio, &tm_envio_handle));
-
 }
 
 // Activacion
-void set_timers(void *arg) {
+void set_timers(void *arg)
+{
     ESP_ERROR_CHECK(esp_timer_start_periodic(tm_lectura_handle, T_LECTURA * 1000));
     ESP_ERROR_CHECK(esp_timer_start_periodic(tm_envio_handle, T_ENVIO * 1000));
+    xEventGroupSetBits(timers_event_group, TIMERS_READY_BIT);
 }
-
 
 /* SGP30 */
 // Inicializacion
-static void sgp30_co2_init(void) {
+static void sgp30_co2_init(void)
+{
 
     uint16_t eco2_baseline, tvoc_baseline;
 
@@ -174,30 +180,34 @@ static void sgp30_co2_init(void) {
     // Sensor
     sgp30_init(&sgp30_sensor, (sgp30_read_fptr_t)main_i2c_read, (sgp30_write_fptr_t)main_i2c_write);
 
+    timers_event_group = xEventGroupCreate();
     timers_init();
 
     esp_timer_handle_t start_timer;
 
     const esp_timer_create_args_t args_start_timer = {
         .callback = &set_timers,
-        .name = "Tiempo de inicio"
-    };
+        .name = "Tiempo de inicio"};
 
     ESP_ERROR_CHECK(esp_timer_create(&args_start_timer, &start_timer));
     // Wait 15 seconds before starting the timers (from datasheet)
-    ESP_ERROR_CHECK(esp_timer_start_once(start_timer, 15000000));
+    ESP_ERROR_CHECK(esp_timer_start_once(start_timer, 15000001));
 }
 
-
-void sensor_co2_create(sensor_co2_handler handler){
+void sensor_co2_create(sensor_co2_handler handler)
+{
     sensor_handler = handler;
     sgp30_co2_init();
 }
 
-void sensor_co2_set_frecuencia_lectura(int frec){
-    ESP_ERROR_CHECK(esp_timer_restart(tm_lectura_handle, pdMS_TO_TICKS(frec)));
+void sensor_co2_set_frecuencia_lectura(int frec)
+{
+    xEventGroupWaitBits(timers_event_group, TIMERS_READY_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    ESP_ERROR_CHECK(esp_timer_restart(tm_lectura_handle, frec * 1000));
 }
 
-void sensor_co2_set_frecuencia_envio(int frec) {
-    ESP_ERROR_CHECK(esp_timer_restart(tm_envio_handle, pdMS_TO_TICKS(frec)));
+void sensor_co2_set_frecuencia_envio(int frec)
+{
+    xEventGroupWaitBits(timers_event_group, TIMERS_READY_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    ESP_ERROR_CHECK(esp_timer_restart(tm_envio_handle, frec * 1000));
 }
